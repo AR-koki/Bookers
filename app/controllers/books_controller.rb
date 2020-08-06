59101bfda518d280edbbd7dddb0d936baaf26c71
有澤孝輝 <teruuuuun72@gmail.com>
2020-08-06 20:06:34 +0900

    [Add] bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,29 @@
 class BooksController < ApplicationController
+  def top
+  end
+
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
+  test "should get top" do
+    get books_top_url
+    assert_response :success
+  end
+
   test "should get index" do
     get books_index_url
     assert_response :success
